09c1dfc68b08e71f92072b34af758f2692ef5c0e
Mike Lee <mikemunkyu.lee@gmail.com>
2023-04-14 22:48:56 -0400

    Use layout and test fetching data from server

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,6 @@
+export function load() {
+	console.log('we in server!');
+	return {
+		foo: 'bar'
+	};
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,8 @@
-<div class="flex">
-	<div class="w-[16rem] flex flex-col bg-[#f4f4f4] border-r border-gray-200">
-		<div class="p-2">
-			<span class="font-semibold">sidebar area</span>
-		</div>
-	</div>
-	<div class="flex-1 h-screen relative bg-yellow-100">
-		<div class="p-2">
-			<span class="font-semibold">main area</span>
-		</div>
-	</div>
+<script>
+	export let data;
+	console.log(data);
+</script>
+
+<div class="p-2">
+	<span class="font-semibold">main area</span>
 </div>
