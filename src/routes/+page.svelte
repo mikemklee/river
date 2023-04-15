bfd4932ee229f8229c2b306d1f9b86d67736cead
Mike Lee <mikemunkyu.lee@gmail.com>
2023-04-14 21:51:41 -0400

    Scaffold app areas

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p class="bg-red-100">
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<div class="flex">
+	<div class="w-[16rem] flex flex-col bg-[#f4f4f4] border-r border-gray-200">
+		<div class="p-2">
+			<span class="font-semibold">sidebar area</span>
+		</div>
+	</div>
+	<div class="flex-1 h-screen relative bg-yellow-100">
+		<div class="p-2">
+			<span class="font-semibold">main area</span>
+		</div>
+	</div>
+</div>
