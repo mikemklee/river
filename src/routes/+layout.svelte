d78b30269b9e5060128a00b63153a0fe21fd012a
Mike Lee <mikemunkyu.lee@gmail.com>
2023-04-14 22:48:26 -0400

    Add layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,13 @@
 	import '../app.css';
 </script>
 
-<slot />
+<div class="flex">
+	<nav class="w-[16rem] flex flex-col bg-[#f4f4f4] border-r border-gray-200">
+		<div class="p-2">
+			<span class="font-semibold">sidebar area</span>
+		</div>
+	</nav>
+	<div class="flex-1 h-screen relative bg-yellow-100">
+		<slot />
+	</div>
+</div>
